bd2bfa0b80bdf290baf92a86adea647116f2b694
SamuGasto <samuel.55321@gmail.com>
2024-07-24 18:38:21 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Make&nbsp;</h1>
-          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+          <h1 className={title()}>Samuel&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>Ramírez&nbsp;</h1>
           <br />
           <h1 className={title()}>
             websites regardless of your design experience.
