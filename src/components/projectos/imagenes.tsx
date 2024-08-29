be52b3b97accf2b6666bb1406f8719a2724621a4
SamuGasto <samuel.55321@gmail.com>
2024-08-29 00:40:33 -0400

    Corrección de bugs carrousel

--- src/components/projectos/imagenes.tsx
+++ src/components/projectos/imagenes.tsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Image } from "@nextui-org/image";
 import { Button } from "@nextui-org/button";
+import { useState } from "react";
 
 interface PropType {
   info: Analisis | Aplicacion;
@@ -12,51 +13,63 @@ interface PropType {
 
 function Imagenes(props: PropType) {
   const { info } = props;
+  const imagenes = [info.imagenPrincipalURL].concat(info.imagenesExtra);
 
-  function NextArrow(props: any) {
-    const { onClick } = props;
+  const [index, setIndex] = useState(0);
+
+  function NextArrow() {
     return (
       <div className="p-1 lg:p-5 justify-center self-center">
-        <Button size="sm" variant="bordered" onClick={onClick}>
+        <Button
+          size="sm"
+          variant="bordered"
+          onClick={() => {
+            if (index === imagenes.length - 1) {
+              setIndex(0);
+              return;
+            } else setIndex(index + 1);
+          }}
+        >
           {">"}
         </Button>
       </div>
     );
   }
-  function PrevArrow(props: any) {
-    const { onClick } = props;
+  function PrevArrow() {
     return (
       <div className="p-1 lg:p-5 justify-center self-center">
-        <Button size="sm" variant="bordered" onClick={onClick}>
+        <Button
+          size="sm"
+          variant="bordered"
+          onClick={() => {
+            console.log(index);
+            if (index === 0) {
+              setIndex(imagenes.length - 1);
+              return;
+            } else setIndex(index - 1);
+          }}
+        >
           {"<"}
         </Button>
       </div>
     );
   }
 
+  function Carousel() {
+    return (
+      <div className="flex flex-col lg:flex-row w-full">
+        <PrevArrow />
+        <Image src={imagenes[index]} />
+        <NextArrow />
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col w-ful gap-8 items-center">
+    <div className="flex flex-col w-full gap-8 items-center">
       <h1 className="font-PlaywriteDKUloopet text-4xl">Imágenes</h1>
       <h1 className="text-lg">{info.textoImagen}</h1>
-      <Slider
-        className="flex w-full"
-        dots={false}
-        infinite={true}
-        speed={500}
-        slidesToShow={1}
-        slidesToScroll={1}
-        prevArrow={<PrevArrow />}
-        nextArrow={<NextArrow />}
-      >
-        <div>
-          <Image src={info.imagenPrincipalURL} />
-        </div>
-        {info.imagenesExtra.map((imagen, index) => (
-          <div>
-            <Image key={index} src={imagen} />
-          </div>
-        ))}
-      </Slider>
+      <Carousel />
     </div>
   );
 }
